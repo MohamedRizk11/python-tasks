4a0d8775667aa846c7f064d95f4fca1eee37faf6
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-16 21:28:07 +0300

    task7

---
+++ 07-task.py
@@ -0,0 +1,24 @@
+"""
+Create a function takes a sentence and a word and prints how many time the word used in the sentence
+
+"""
+s = str(input("inter your sentance :"))
+x = str(input("inter the word :"))
+sy=s.count(x)
+print(sy)
+
+
+def countwords(v):
+    b = str(input("inter your sentance :"))
+    v = str(input("inter the word :"))
+    if v in b:
+     result= b.count(v)
+
+
+
+
+
+
+
+
+countwords()    
