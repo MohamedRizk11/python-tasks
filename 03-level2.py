f90a3805f4017147a0915b9f8fde0a2e99fa8b9b
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-17 01:52:45 +0300

    task 3

--- 03-level2.py
+++ 03-level2.py
@@ -4,4 +4,26 @@ Get the names that starts with t in a list using [normal list - list comprehensi
 programming]
 
 """
-names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"]
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"] 
+startword="m"
+# normal list
+result1=[]
+
+for x in names:
+    if x[0]==startword:
+        result1.append(x)
+print (result1)  
+
+# list comprehension
+
+result2=[x for x in names if x[0].lower()== startword.lower()]
+print(result2)
+
+# functional programming
+def filterword(n):
+    if  n[0]==startword:
+        return n
+    
+result3=list(filter(filterword,names))
+print(result3)
+    
