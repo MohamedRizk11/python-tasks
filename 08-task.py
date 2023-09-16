5d846fb03e7628315f5ab10908f9943acf519c6e
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-16 21:44:27 +0300

    task 8

---
+++ 08-task.py
@@ -0,0 +1,10 @@
+"""
+Create a function that takes x,y and prints all the number that can be divide by y from x to 100
+
+"""
+
+x = int(input("enter first number :"))
+y = int(input("enter secound number :"))
+for c in range(x,100):
+    if c%y==0:
+        print (list(c))
