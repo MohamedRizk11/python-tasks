e3716377d741945efd959842fb4df3fcacfa4db8
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-17 00:33:54 +0300

    task 3

---
+++ 03-level2.py
@@ -0,0 +1,7 @@
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"]
+Get the names that starts with t in a list using [normal list - list comprehension - functional
+programming]
+
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"]
