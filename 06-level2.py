e9889925e298e221ba6d9fb8d721b6ad91526c66
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-17 03:14:28 +0300

    task 6

---
+++ 06-level2.py
@@ -0,0 +1,9 @@
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"] 
+
+Unpack the list in
+
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"] 
+for _ in range(10):
+    print(names)
