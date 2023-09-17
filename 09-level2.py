dc1f79133a5a90e4f9d9beb9aca7f8378e460ca2
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-17 03:34:26 +0300

    task9

---
+++ 09-level2.py
@@ -0,0 +1,9 @@
+"""
+names =['mahmoud', 'farida', 'ali', 'hassan', 'mohamed', 'khaled', 'taha']
+
+a = the first index , b = the second index
+
+"""
+names =['mahmoud', 'farida', 'ali', 'hassan', 'mohamed', 'khaled', 'taha']
+a,b,*z=names
+print (a,b)
