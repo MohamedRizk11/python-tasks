03457a54da6ffa41d28468a4ea012b6c1e8a5397
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-09-17 03:11:05 +0300

    task5

---
+++ 05-level2.py
@@ -0,0 +1,25 @@
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"] 
+
+Print a list contains the len of each word in the list using [normal list - list comprehension -
+functional programming]
+
+"""
+names = ["mahmoud","farida","ali","hassan","mohamed","khaled","taha"] 
+
+# normal list
+
+for x in  names :
+  print ( len(x))
+
+# list comprehension
+
+result1=[len(x) for x in names ]
+print(result1)
+
+# functional programming
+def countlen(names):
+    for x in names:
+        print(len(x))
+
+countlen(names)        
